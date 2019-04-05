e76b2feb2690ca6b5a9dbb29ef5cb4c761ce85fc
acnagle <acnagle@wisc.edu>
2019-04-05 08:20:06 -0500

    processed hourly data and labels; developing code for model evaluation

--- Code/Format_Hourly_Data.py
+++ Code/Format_Hourly_Data.py
@@ -12,13 +12,16 @@ def main():
     # read in arguments from command line
     try:
         cla_path = str(sys.argv[1])
-        weather_path = str(sys.argv[2])
+        label_path = str(sys.argv[2])
+        weather_path = str(sys.argv[3])
 
         cla_data = np.load(cla_path)
+        labels = np.load(label_path)
         weather_data = np.load(weather_path)
     except (ValueError, IndexError, FileNotFoundError):
         print('Arguments must be specified as follows:')
-        print('python3 format_hourly_data.py <path-to-cla-data (.npy)> <path-to-weather-data (.npy)>')
+        print('python3 format_hourly_data.py <path-to-cla-data (.npy)> <path-to-cla-data-labels (.npy)> '
+              '<path-to-weather-data (.npy)>')
         sys.exit(0)
 
     # get initial index of weather data for this data set
@@ -31,15 +34,19 @@ def main():
 
     print('Removing non-summer months ... ')
     # remove non-summer months. all data points must be in June, July, August
-    summer_cla_data = np.zeros(shape=(cla_data.shape[1]))
+    summer_cla_data = np.array([])
+    summer_labels = np.array([])
 
     for i in range(cla_data.shape[0]-1, 0, -1):
         cla_month = int(cla_data[i, 1].split('/')[0])
 
         if (cla_month == 6) or (cla_month == 7) or (cla_month == 8):
-            summer_cla_data = np.vstack((summer_cla_data, cla_data[i, :]))
+            if summer_cla_data.shape[0] == 0:
+                summer_cla_data = cla_data[i, :]
+            else:
+                summer_cla_data = np.vstack((summer_cla_data, cla_data[i, :]))
 
-    summer_cla_data = np.delete(summer_cla_data, obj=0, axis=0)
+            summer_labels = np.append(summer_labels, labels[i])
 
     print('Sorting CLA data ... ')
     # sort cla_data by dates
@@ -54,6 +61,7 @@ def main():
 
     date_to_num_arr = date_to_num_arr[sorted_idx]
     summer_cla_data = summer_cla_data[sorted_idx, :]
+    summer_labels = summer_labels[sorted_idx]
 
     print('Appending weather data ... ')
     # append weather data to cla data
@@ -90,10 +98,15 @@ def main():
                     weather_time = weather_data[weather_idx_day, 0].split(' ')[1].split(':')
                     weather_time_to_num = float(weather_time[0]) + (float(weather_time[1]) / 100)
 
-                weather_time = weather_data[weather_idx_day-1, 0].split(' ')[1].split(':')
-                weather_time_to_num = float(weather_time[0]) + (float(weather_time[1]) / 100)
+                # weather_time = weather_data[weather_idx_day-1, 0].split(' ')[1].split(':')
+                # weather_time_to_num = float(weather_time[0]) + (float(weather_time[1]) / 100)
 
                 # print(date_to_num_arr[cla_idx], weather_date_to_num, cla_time_to_num, weather_time_to_num)
+
+                weather_idx_day -= 1
+                print(str(weather_data[weather_idx_day, -2]))
+                if 's' in str(weather_data[weather_idx_day, -1]):
+                    print('found s!!!!!')
 
                 data[cla_idx, 5:] = weather_data[weather_idx_day, 2:]
 
@@ -106,7 +119,8 @@ def main():
 
                 cla_idx += 1
 
-    data = np.delete(data, obj=data.shape[0]-1, axis=0)
+    data = np.delete(data, obj=-1, axis=0)
+    summer_labels = np.delete(summer_labels, obj=-1)
 
     print('Converting time to float values ... ')
     # convert time to float value
@@ -125,9 +139,11 @@ def main():
             if '' in data[i, :]:
                 data = np.delete(data, obj=i, axis=0)
 
-    filename = 'hourly_' + cla_path.split('/')[-1]
+    filename_data = 'hourly_' + cla_path.split('/')[-1]
+    filename_labels = 'hourly_' + label_path.split('/')[-1]
 
-    np.save('../Data/hourly-data-sets/' + filename, data)
+    # np.save('../Data/hourly-data-sets/' + filename_data, data)
+    # np.save('../Data/hourly-data-sets/' + filename_labels, summer_labels)
 
 
 if __name__ == "__main__": main()
