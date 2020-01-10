b5cb1bfe28b11be8e3312df2f0bce9045be3c5c9
Alliot Nagle <Alliot@Alliots-MacBook-Pro.local>
2020-01-10 15:21:01 -0600

    Added ResNet9; adjust num channels

--- Mendota/resnet.py
+++ Mendota/resnet.py
@@ -69,7 +69,10 @@ class ResNet(nn.Module):
         super(ResNet, self).__init__()
         self.in_planes = 64
 
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv1 = nn.Conv2d(1, 64, kernel_size=3, stride=1, padding=1, bias=False)
+        # TODO could make the input image start out with 4 channels, where each channel is the same as the first channel, but rotated by 90, 180, and 270 degrees.
+        # TODO Also may want to adjust kernel size to 2.
+        
         self.bn1 = nn.BatchNorm2d(64)
         self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
         self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
@@ -96,6 +99,9 @@ class ResNet(nn.Module):
         out = self.linear(out)
         return out
 
+    
+def ResNet9():
+    return ResNet(BasicBlock, [1,1,1,1])
 
 def ResNet18():
     return ResNet(BasicBlock, [2,2,2,2])
